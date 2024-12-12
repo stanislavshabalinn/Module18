93c9d5038d395b59c0f19891f66a8c365582f5d6
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-12-12 16:41:22 +0700

    Добавьте файлы проекта.

---
+++ Module18.FinalTask/Cender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module18.FinalTask
+{
+    /// <summary>
+    /// Отправитель команды
+    /// </summary>
+
+    class Sender
+    {
+        Command command;
+
+        public void SetCommand(Command command)
+        {
+            this.command = command;
+        }
+
+        // Выполнить
+        public void Download()
+        {
+            command.Cancel();
+        }
+
+        public void GetInfo()
+        {
+            command.Run();
+        }
+
+    }
+}
---
+++ Module18.FinalTask/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module18.FinalTask
+{
+    /// <summary>
+    /// Базовый класс команды
+    /// </summary>
+    abstract class Command
+    {
+        public abstract void Run();
+        public abstract void Cancel();
+    }
+}
